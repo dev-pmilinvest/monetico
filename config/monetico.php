7cf16478056b06e90e5194a34a71e02bbfb74c52
Benoit DAUBA <contact@dauba.net>
2022-02-11 21:29:41 +0100

    reduce dependencies

--- config/monetico.php
+++ config/monetico.php
@@ -4,11 +4,6 @@
  * Configuration  Monetico
  */
 return [
-    'service_version' => env('MONETICO_SERVICE_VERSION', '3.0'),
-    'main_request_url' => env('MONETICO_MAIN_REQUEST_URL',  'https://p.monetico-services.com'),
-    'misc_request_url' => env('MONETICO_MISC_REQUEST_URLL',  'https://payment-api.e-i.com'),
-
-    /* OBLIGATIORE dans les fichier .env */
 
     'ept_code' => env('MONETICO_EPT_CODE', null),
     'security_key' => env('MONETICO_SECURITY_KEY', null),
